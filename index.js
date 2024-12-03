a80ef478bd0859feb1dbd1c4ea9fbc28ddba3d0e
DevilShine <danokpro228@gmail.com>
2024-12-03 16:40:14 +0200

    new conect

---
+++ index.js
@@ -0,0 +1,39 @@
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+var ships = ['1-a', '3-a', '4-a', '6-b', '2-c', '6-c', '2-d', '2-e', '6-f', '7-f', '8-f', '1-h', '2-h', '3-h', '4-h'];
+
+function createBtn(i, letter) {
+  var btn = document.createElement("button");
+  btn.classList.add("button");
+  btn.setAttribute("data-id", i + "-" + letter);
+  btn.innerHTML = i + " " + letter;
+  var wrap = document.querySelector(".wrap");
+  wrap.append(btn)
+}
+
+for (var i = 0; i < 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    createBtn(j, alphabet[i])
+  }
+}
+
+var wrap = document.querySelector(".wrap");
+wrap.addEventListener("click", function (e) {
+  var elem = e.target;
+  var check = elem.matches(".button");
+  if (check) {
+    var ch;
+    for (let i = 0; i < ships.length; i++) {
+      if (ships[i] == elem.dataset.id) {
+        ch = "true";
+        break;
+      } else {
+        ch = "false";
+      }
+    }
+    if (ch == "true") {
+      alert("Nice Shot)");
+    } else {
+      alert("Miss((");
+    }
+  }
+})
